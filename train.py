1f971abafc0f337caf544d37ec38eef8b5207a0f
breadbread1984 <breadbread1984@163.com>
2021-10-27 09:31:45 +0800

    update

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def main(unused_argv):
   # 3) optimizer
   callbacks = [
     tf.keras.callbacks.TensorBoard(log_dir = FLAGS.checkpoint),
-    tf.keras.callbacks.ModelCheckpoint(file_path = join(FLAGS.checkpoint, 'ckpt'), save_freq = FLAGS.checkpoint_steps),
+    tf.keras.callbacks.ModelCheckpoint(filepath = join(FLAGS.checkpoint, 'ckpt'), save_freq = FLAGS.checkpoint_steps),
     SummaryCallback(dasr, FLAGS.eval_steps),
   ];
   dasr.fit(trainset, epochs = FLAGS.epochs, validation_data = testset, callbacks = callbacks);
