84175e3095ca4b0ffbfae60a19130bef18235eaf
breadbread1984 <breadbread1984@163.com>
2021-11-09 10:23:04 +0800

    update

--- save_model.py
+++ save_model.py
@@ -12,9 +12,19 @@ def add_options():
   flags.DEFINE_enum('scale', default = '2', enum_values = {'2', '3', '4'}, help = 'scale of resolution');
 
 def main(unused_argv):
+  # 1) load from checkpoints
   dasr = tf.keras.models.load_model(join(FLAGS.checkpoint, 'dasr_ckpt'), custom_objects = {'tf': tf}, compile = True);
+  # 2) extract trained layers and recompile a new model
+  moco = dasr.get_layer('moco');
+  sr = dasr.get_layer('sr');
+  encoder = moco.encoder_q;
+  inputs = tf.keras.Input((None, None, 3));
+  da_embedding, _ = encoder(inputs);
+  results = sr([inputs, da_embedding]);
+  dasr = tf.keras.Model(inputs = inputs, outputs = results);
+  # 3) save model
   if not exists('models'): mkdir('models');
-  dasr.save_weights(join('models', 'dasr_x%s_weights.h5' % FLAGS.scale));
+  dasr.save(join('models', 'dasr_x%s.h5' % FLAGS.scale));
 
 if __name__ == "__main__":
   add_options();
